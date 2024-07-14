b076174e86d59e019de58663d869baa60f79b29b
Ashish Bhosle <asbhosle2003@gmail.com>
2024-07-14 17:15:31 +0530

    Delete head of DLL

--- DLL/DLL.cpp
+++ DLL/DLL.cpp
@@ -45,3 +45,20 @@ int main() {
     print (head);
     return 0;
 }
+
+// Delete Head of a Doubly Linked List
+
+Node * deleteHead(Node *head) {
+    // Write your code here.
+    if (head== NULL || head->next== NULL){
+        return NULL;
+    }
+    Node* prev = head;
+    head= head->next;
+
+    head->prev = nullptr;
+    prev->next = nullptr;
+
+    delete prev;
+    return head;
+}
